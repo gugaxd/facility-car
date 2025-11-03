7923f1020b921ddfcf487d57215b5f600a73f505
Gustavo Henrique Copetti <92458392+gugaxd@users.noreply.github.com>
2025-11-03 16:30:25 -0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -16,7 +16,7 @@ const Index = () => {
             backgroundPosition: 'center',
           }}
         />
-        <div className="container h-20 text-center text-white">
+       <div className="container mx-auto px-4 z-10 text-center text-white">
           <img src="/logo.png" alt="Facility Car" className="h-32 w-auto" />
           <p className="text-2xl md:text-3xl mb-4 font-light">
             Mobilidade para Todos
